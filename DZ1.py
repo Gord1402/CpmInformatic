50f538feefe5f17bf77a9061b706fa95f5cdb4cf
Gord1402 <81515333+Gord1402@users.noreply.github.com>
2023-10-17 11:37:39 +0300

    DZ1.py

---
+++ DZ1.py
@@ -0,0 +1,12 @@
+x = float(input())
+if x > 0:
+    print("x > 0")
+elif x < 0:
+    print("x < 0")
+else:
+    print("x = 0")
+
+if x % 1 == 0:
+    print("Целочисленное")
+else:
+    print("Дробное")
