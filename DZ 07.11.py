043bddaa9a99fd9c10efa10b6191a85d8531d360
Gord1402 <81515333+Gord1402@users.noreply.github.com>
2023-11-04 16:38:05 +0300

    DZ1.py

---
+++ DZ 07.11.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+a, b = int(input()), int(input())
+
+for number in range(a, b + 1):
+    divisors = []
+    for divider in range(2, int(sqrt(number)) + 1):
+        if number % divider == 0:
+            if len(divisors) >= 4:
+                break
+            divisors.append(divider)
+            if number // divider != divider:
+                divisors.append(number // divider)
+    else:
+        if len(divisors) < 4:
+            continue
+        print(number, "|", " ".join(map(str, reversed(divisors))))
